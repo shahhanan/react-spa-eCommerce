0fae2c249368dd58bb90ac7444e4dd497e8836d5
@HananShah <hanan.shah@mulesoft.com>
2018-06-19 03:04:12 +0530

    migrated to redux with all filter proprts working

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,38 +1,124 @@
 import React, { Component } from "react";
-import LeftSide from "./leftSide/LeftSide";
+import LeftSide from "./leftSide";
 import RightSide from "./rightSide/RightSide";
-import { getTrendngProducts, getAllProducts } from '../../actions/products';
-import { connect } from 'react-redux';
+import { getTrendngProducts, getAllProducts } from "../../actions/products";
+import { connect } from "react-redux";
 
 class Body extends Component {
-  
-  componentWillMount(){
-      this.props.getTrendngProducts();
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayProducts: [],
+      filterProperties: {
+        category: null,
+        categoryName: "",
+        pricing: {
+          currentValue:[0, 1000],   
+          step:1,
+          min: 0,
+          max: 1000
+        }
+      }
+    };
   }
-/*  setCategory = selectedCategory => {
+
+  componentWillMount = () => {
+    this.props.getAllProducts();
+    this.props.getTrendngProducts();
+  }
+  componentWillReceiveProps = (nextProps) =>{
+    let state = JSON.parse(JSON.stringify(this.state));
+    state.displayProducts = nextProps.trendingProducts;
+    state.filterProperties.categoryName=  "Trending Products";
+    this.setState(state); 
+  }
+  setCategory = selectedCategory => {
+    let  data  = this.props.allProducts;
+    let products = [];
+        data.map((data, index) => {
+            if(data.category == selectedCategory){
+             products.push(data);
+            }
+            
+          });  
     let state = JSON.parse(JSON.stringify(this.state));
     state.filterProperties.category = selectedCategory;
+    state.displayProducts = products;
+    state.filterProperties.categoryName = selectedCategory;
     this.setState(state);
   };
+  setElectronics = () => {
+    let state = JSON.parse(JSON.stringify(this.state));
+    state.displayProducts = this.props.allProducts;
+    state.filterProperties.pricing.category = null;
+    state.filterProperties.pricing.minPrice = 0;
+    state.filterProperties.pricing.maxprice = 1000;
+    this.setState(state);
+  };
+  filteredPricing = (data) =>{
+    let response = [];
+    let min = this.state.filterProperties.pricing.currentValue[0];
+    let max = this.state.filterProperties.pricing.currentValue[1];
+    data.map((data, index) => {
+      if(data.price > min && data.price < max){
+        response.push(data);
+      }
+    });  
+    return response;
+  }
+    
   setPricing = selectedPrice => {
     let state = JSON.parse(JSON.stringify(this.state));
     state.filterProperties.pricing = selectedPrice;
     this.setState(state);
   };
   setReset = () => {
-    this.setState({ filterProperties: { category: null, pricing: [null, null] } });
-  };   */
+    let state = JSON.parse(JSON.stringify(this.state));
+    state.displayProducts = this.props.trendingProducts;
+    state.filterProperties.pricing.category = null;
+    state.filterProperties.pricing.min = 0;
+    state.filterProperties.pricing.max = 1000;
+    state.filterProperties.pricing.currentValue[0] = 0;
+    state.filterProperties.pricing.currentValue[1] = 1000;
+    state.filterProperties.categoryName=  "Trending Products";
+    this.setState(state);
+  };   
+  changefilterValue = (min, max) =>{
+    let state = JSON.parse(JSON.stringify(this.state));
+    state.filterProperties.pricing.currentValue[0] = min;
+    state.filterProperties.pricing.currentValue[1] = max;
+    this.setState(state);
+  }
   render() {
-   
-      return (
-        <div>
-          <div className="row lightestgrey">
-            <LeftSide/>
-            <RightSide />
-          </div>
+    let products = this.props.allProducts;
+    let data = this.filteredPricing(this.state.displayProducts);
+    let productsData = data
+    let filterData = this.state.filterProperties.pricing;
+    return (
+      <div>
+        <div className="row lightestgrey">
+          <LeftSide products={products} 
+          setCategory={this.setCategory} 
+          setElectronics={this.setElectronics}
+          setReset={this.setReset}
+          changefilterValue={this.changefilterValue}
+          filterData={filterData}
+          />
+          <RightSide 
+          products={productsData}
+          categoryName={this.state.filterProperties.categoryName}
+          />
         </div>
-      );
-    }
+      </div>
+    );
+  }
 }
+const mapStateToprops = state => ({
+  allProducts: state.products.allProducts,
+  trendingProducts: state.products.trendingItems
+});
 
-export default connect (null, { getTrendngProducts })(Body);
+export default connect(
+  mapStateToprops,
+  { getTrendngProducts, getAllProducts }
+)(Body);
--- src/components/body/rightSide/RightSide.js
+++ src/components/body/rightSide/RightSide.js
@@ -6,56 +6,69 @@ class ListItem extends Component {
   }
 
   render() {
-
-         let { data } = this.props  ;
-        let populate = data.map((data, index) => {
-            let image = null;
-            if(data.category == 'Mobile'){
-             image = 'mobile.jpg';
-            }
-            else{
-             image = 'laptop.png';
-            }
+    if (this.props.products !== undefined) {
+      let  products  = this.props.products;
+      let populate = []; 
+      populate = products.map((products, index) => {
+        let image = null;
+        if (products.category == "Mobile") {
+          image = "mobile.jpg";
+        } else {
+          image = "laptop.png";
+        }
         return (
-            <li className="item" key={index}>
+          <li className="item" key={index}>
             <div className="front">
-                <div className="frontimage"> 
-                    <img className="img-fluid" src={"./assets/images/"+image} /> 
-                </div>
-                <div className="info">
-                <header  style={{backgroundImage:'url(./assets/images/still01.jpg)'}}>
-                    <h1 className="itemname" key={data.id} > {data.company} </h1>
-                    <span className="date">{}</span>
-                    <span className="companyinitials">{data.model}</span>
-                    <span className="more">Price Rs->{data.price}</span>
+              <div className="frontimage">
+                <img className="img-fluid" src={"./assets/images/" + image} />
+              </div>
+              <div className="info">
+                <header
+                  style={{
+                    backgroundImage: "url(./assets/images/still01.jpg)"
+                  }}
+                >
+                  <h1 className="itemname" key={products.id}>
+                    {" "}
+                    {products.company}{" "}
+                  </h1>
+                  <span className="date">{}</span>
+                  <span className="companyinitials">{products.model}</span>
+                  <span className="more">Price Rs->{products.price}</span>
                 </header>
-                <p>Description: {data.category}</p>
+                <p>Description: {products.category}</p>
                 <p>
-                    <button>View</button>
-                    <button>Save</button>
-                    <button>Buy Now</button>
+                  <button>View</button>
+                  <button>Save</button>
+                  <button>Buy Now</button>
                 </p>
-                </div>
+              </div>
             </div>
-            </li>
+          </li>
         );
-        }); 
-    return(
-       <div> {populate} </div>     
-    );
+      });
+      return <div> {populate} </div>;
+    } else {
+      return <div />;
+    }
   }
 }
 
 class RightSide extends Component {
   render() {
-    let { data } = this.props;
+    let { products } = this.props;
+    if(this.props.categoryName != undefined){
+      var Name = this.props.categoryName;
+    }
+    
     return (
       <div className="col-md-10 order-md-2">
         <div className="g-pl-15--lg">
           <div className="g-brd-bottom g-brd-gray-light-v4">
             <ul className="group clearfix g-bg-custom">
-          <span className="row justify-content-center c-white" > </span>
-              <ListItem data={data} />
+              <span className="row justify-content-center c-white"> </span>
+              <div className="col "style={{ color:'black'}} align="center"> {Name} </div>
+              <ListItem products={products} />
             </ul>
           </div>
         </div>
